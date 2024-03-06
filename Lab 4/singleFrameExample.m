0dec49c19db6f8777d88190e4f6681f50c724f41
Yin <55523416+anh1213@users.noreply.github.com>
2024-03-06 10:28:16 -0500

    Add files via upload
    be sure to add these files to the mathworks-unity-link file structure so that they run

--- Lab 4/singleFrameExample.m
+++ Lab 4/singleFrameExample.m
@@ -12,43 +12,125 @@ server_port = 55001;           %Server Port of the Unity Sever
 client = tcpclient(server_ip,server_port);
 fprintf(1,"Connected to server\n");
 
-depth = 1.25
+depth = 0
 % x(in out),y(left right),z(up down),yaw[z],pitch[y],roll[x]
 pose = [depth,-0.03,0,0,0,0];
 unityImageLeft = unityLink(client,pose);
 %figure out why the image is squished
-subplot(1,4,1)
-imshow(unityImageLeft);
+subplot(3,4,1)
+imshow(unityImageLeft)
 
-Frame_1 = unityImageLeft
+Frame_1 = unityImageLeft;
 formated_frame_1 = rgb2gray(Frame_1);
 colormap gray
 imagesc(formated_frame_1)
 
-subplot(1,4,3)
+subplot(3,4,3)
 imshow(formated_frame_1)
-[center, radius] = imfindcircles(formated_frame_1,[25 75],"Sensitivity",0.85)
+[center, radius] = imfindcircles(formated_frame_1,[25 75],"Sensitivity",0.80)
 hold on
 viscircles(center, radius, 'Color', 'red')
 scatter(center(1), center(2))
 
+%%% 2
 pose = [depth,0.03,0,0,0,0];
 unityImageRight = unityLink(client,pose);
 %
-subplot(1,4,2)
+subplot(3,4,2)
 imshow(unityImageRight);
 
-Frame_2 = unityImageRight
+Frame_2 = unityImageRight;
 formated_frame_2 = rgb2gray(Frame_2);
 colormap gray
 imagesc(formated_frame_2)
 
-subplot(1,4,4)
+subplot(3,4,4)
 imshow(formated_frame_2)
-[center, radius] = imfindcircles(formated_frame_2,[25 75],"Sensitivity",0.85)
+[center, radius] = imfindcircles(formated_frame_2,[25 75],"Sensitivity",0.80)
 hold on
 viscircles(center, radius, 'Color', 'red')
 scatter(center(1), center(2))
+
+%%% 3
+%depth = -1.25
+% x(in out),y(left right),z(up down),yaw[z],pitch[y],roll[x]
+pose = [depth,-0.03,0,0,0,0];
+unityImageLeft = unityLink(client,pose);
+%figure out why the image is squished
+subplot(3,4,5)
+imshow(unityImageLeft)
+
+Frame_1 = unityImageLeft;
+formated_frame_1 = rgb2gray(Frame_1);
+colormap gray
+imagesc(formated_frame_1)
+
+subplot(3,4,7)
+imshow(formated_frame_1)
+[center, radius] = imfindcircles(formated_frame_1,[25 75],"Sensitivity",0.80)
+hold on
+viscircles(center, radius, 'Color', 'red')
+scatter(center(1), center(2))
+
+%%% 4
+pose = [depth,0.03,0,0,0,0];
+unityImageRight = unityLink(client,pose);
+%
+subplot(3,4,6)
+imshow(unityImageRight);
+
+Frame_2 = unityImageRight;
+formated_frame_2 = rgb2gray(Frame_2);
+colormap gray
+imagesc(formated_frame_2)
+
+subplot(3,4,8)
+imshow(formated_frame_2)
+[center, radius] = imfindcircles(formated_frame_2,[25 75],"Sensitivity",0.80)
+hold on
+viscircles(center, radius, 'Color', 'red')
+scatter(center(1), center(2))
+
+%%% 5
+%depth = -3
+% x(in out),y(left right),z(up down),yaw[z],pitch[y],roll[x]
+pose = [depth,-0.03,0,0,0,0];
+unityImageLeft = unityLink(client,pose);
+%figure out why the image is squished
+subplot(3,4,9)
+imshow(unityImageLeft)
+
+Frame_1 = unityImageLeft;
+formated_frame_1 = rgb2gray(Frame_1);
+colormap gray
+imagesc(formated_frame_1)
+
+subplot(3,4,11)
+imshow(formated_frame_1)
+[center, radius] = imfindcircles(formated_frame_1,[25 75],"Sensitivity",0.80)
+hold on
+viscircles(center, radius, 'Color', 'red')
+scatter(center(1), center(2))
+
+%%% 6
+pose = [depth,0.03,0,0,0,0];
+unityImageRight = unityLink(client,pose);
+%
+subplot(3,4,10)
+imshow(unityImageRight);
+
+Frame_2 = unityImageRight;
+formated_frame_2 = rgb2gray(Frame_2);
+colormap gray
+imagesc(formated_frame_2)
+
+subplot(3,4,12)
+imshow(formated_frame_2)
+[center, radius] = imfindcircles(formated_frame_2,[25 75],"Sensitivity",0.80)
+hold on
+viscircles(center, radius, 'Color', 'red')
+scatter(center(1), center(2))
+
 
 %Close Gracefully
 fprintf(1,"Disconnected from server\n");
